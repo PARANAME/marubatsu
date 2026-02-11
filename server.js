acf827a524805260fc40bad3b8d55dc0b532c64c
なめめぐ <namemegu@namemegunoMacBook-Air.local>
2026-02-11 23:36:29 +0900

    restartボタン

--- server.js
+++ server.js
@@ -80,6 +80,25 @@ io.on("connection", socket => {
     io.to(room).emit("state", game);
   });
 
+  // ===== ゲームリセット =====
+  socket.on("reset", ({room}) => {
+
+    let game = rooms[room];
+    if (!game) return;
+
+    // 全プレイヤーの位置をリセット
+    Object.keys(game.players).forEach(id => {
+      game.players[id].pos = 0;
+    });
+
+    // ゲーム状態をリセット
+    game.finished = false;
+    game.winner = null;
+
+    // 全員に同期
+    io.to(room).emit("state", game);
+  });
+
   // ===== 切断 =====
   socket.on("disconnect", () => {
 
